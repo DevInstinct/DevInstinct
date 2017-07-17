cfd0d8f081f0bfcbabf3d19a92b2063a8813b8d1
Martin Lapierre <martin_lapierre@devinstinct.com>
2017-07-17 19:09:40 -0400

    Added AutoMapper, MediatR nugets

---
+++ src/DevInstinct.AspNetCore.Mvc/Filters/ValidateModelStateAttribute.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace DevInstinct.AspNetCore.Mvc.Filters
+{
+    // https://jonhilton.net/2016/05/23/3-ways-to-keep-your-asp-net-mvc-controllers-thin/
+    public class ValidateModelStateAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            if (!filterContext.ModelState.IsValid)
+            {
+                filterContext.Result = new BadRequestObjectResult(filterContext.ModelState);
+            }
+        }
+    }
+}
