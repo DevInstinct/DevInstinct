89df404a2be65bf529493a0819d68ca09c59d372
Martin Lapierre <martin_lapierre@devinstinct.com>
2017-08-07 13:57:18 -0400

    Renaming

--- src/DevInstinct/Patterns/CQRSPattern/ICommand.cs
+++ src/DevInstinct/Patterns/CQRSPattern/ICommand.cs
@@ -4,6 +4,9 @@
     {
     }
 
+
+    // TODO: should support ICommand<TResult> for real-life scenarios where commands must return results.
+
     public interface ICommand<TModel> : ICommand
     {
         TModel Model { get; set; }
